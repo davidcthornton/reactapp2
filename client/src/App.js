c7f79b9a2a21687a17e3f23eb72030cd87ea4836
davidcthornton <thornton@jsu.edu>
2025-07-08 14:34:10 -0500

    added a couple of buttons

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,13 @@
+// App.js
 import React, { useState, useEffect } from 'react';
-function App() {
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Page1 from './Page1';
+import Page2 from './Page2';
+
+function Home() {
   const [message, setMessage] = useState('');
+  const navigate = useNavigate();
+
   useEffect(() => {
     fetch('http://localhost:5000/')
       .then(response => response.text())
@@ -10,7 +17,20 @@ function App() {
   return (
     <div className="App">
       <h1>{message}</h1>
+      <button onClick={() => navigate('/page1')}>Go to Page 1</button>
+      <button onClick={() => navigate('/page2')}>Go to Page 2</button>
     </div>
   );
 }
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/page1" element={<Page1 />} />
+      <Route path="/page2" element={<Page2 />} />
+    </Routes>
+  );
+}
+
 export default App;
---
+++ client/src/Page2.js
@@ -0,0 +1,12 @@
+// Page2.js
+import React from 'react';
+
+function Page2() {
+  return (
+    <div>
+      <h2>This is Page 2</h2>
+    </div>
+  );
+}
+
+export default Page2;
